dc7b36ae088ab72e1d84459108db3650f269ffd6
Romar Castro <122733274+romarcastro@users.noreply.github.com>
2024-09-12 15:48:13 +0800

    Set-up Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import TestPage from '../pages/TestPage'
+import { Route, Routes } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <Routes>
+
+    </Routes>
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
