c7e16b38bb779fce1eadde218287ddb9a7ac6080
rrosco <rrosco@gbox.adnu.edu.ph>
2024-09-15 01:31:46 +0800

    Login and Logout Script Done

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -1,13 +1,46 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../config/firebase.js"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "../config/firebase.js";
 
 export const register = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        console.log(email + " " + password);
-        await createUserWithEmailAndPassword(auth, email, password);
-        res.status(201).json({success: true, message: "Admin Account Created"});
-    } catch (e) {
-        console.log(e);
-    }
-}
+  const { email, password } = req.body;
+  console.log(email + " " + password);
+  await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+  res.status(201).json({ success: true, message: "Admin Account Created" });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+  res.status(201).json({ success: true, message: "Login Success" });
+};
+
+export const logout = async (req, res) => {
+  signOut(auth)
+    .then(() => {
+      res.status(201).json({ success: true, message: "Logout Success" });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+};
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { register } from '../controller/adminController.js';
+import { register, login, logout } from '../controller/adminController.js';
 
 const router = express.Router();
 
 // register
-router.post("/", register);
+router.post("/register", register);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
 
 export default router;
