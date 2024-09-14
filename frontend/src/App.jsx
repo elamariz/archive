31b56ff34108877e9d7b74603e4b281ba431d4e9
Romar Castro <122733274+romarcastro@users.noreply.github.com>
2024-09-14 23:59:37 +0800

    Added Pages and Components
    Added pages and started with the landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState } from "react";
 import { Route, Routes } from 'react-router-dom'
-
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    {/* Landing Page */}
+    <div className="content">
+      <Navbar />
+
+      <div className="searchbar">
+        <span className='Display'>Search the arc<span className='Display_2'>hive</span></span>
+      </div>
+      {/* <Search /> */}
+
+      {/* <Footer /> */}
+    </div>
     </>
   )
 }
